af484d94380c17ff0e258c97db1a558c61b4fa8b
Divyansh Agarwal <dagarwal058@gmail.com>
2023-12-12 19:42:53 +0530

    Recipe structure done

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Recipe } from '../recipes.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[]   = [
-    new Recipe('burger', 'a delicious burger', 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=999&q=80'),
-    new Recipe('burger 2', 'a very delicious burger', 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=999&q=80')
+    new Recipe(101,'burger', 'a delicious burger', 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=999&q=80'),
+    new Recipe(102,'burger 2', 'a very delicious burger', 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=999&q=80')
   ]
 
   current: number = 0;
--- src/app/components/recipes/recipes.model.ts
+++ src/app/components/recipes/recipes.model.ts
@@ -1,9 +1,11 @@
 export class Recipe{
+    public id: number;
     public name: string;
     public description: string;
     public imagePath: string;
 
-    constructor(name: string, description: string, imagePath: string){
+    constructor(id: number, name: string, description: string, imagePath: string){
+        this.id = id;
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
